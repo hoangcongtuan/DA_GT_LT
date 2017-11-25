5f067099ed8daa7beae9e17cc88f9a21381246b2
CongTuan <congtuanspm97@gmail.com>
2017-11-25 17:24:49 +0700

    lam viec voi tep, toi uu thuat toan

--- ToHop/main.cpp
+++ ToHop/main.cpp
@@ -1,70 +1,86 @@
 #include <iostream>
+#include <fstream>
 #define MAX 1000
 
 using namespace std;
 
+//mang quy hoach dong arr[i, j] = C[i, j] = C[n, k]
 long long **arr;
+const char * FILE_INPUT = "input.txt";
+const char * FILE_OUTPUT = "output.txt";
+long long n;
+long long k;
+long long res;
 
-int toHop(int k, int n) {
-	if (k > n)
-		return 0;
-	if (n == 0)
-		return 0;
-	if (k == 0)
-		return 1;
-	if (k == n)
-		return 1;
-	return toHop(k - 1, n - 1) + toHop(k, n - 1);
+
+void loadData() {
+	fstream file;
+	file.open(FILE_INPUT, ios::in);
+	
+	if(!file.is_open()) {
+		cout << "Loi khi mo file! Exit!";
+		exit(0);
+	}
+	
+	file >> n;
+	file >> k;
+	
+	file.close();
+	
+}
+
+void writeResult() {
+	fstream file;
+	file.open(FILE_OUTPUT, ios::out);
+	
+	if(!file.is_open()) {
+		cout << "Loi khi mo file! Exit!";
+		exit(0);
+	}
+	
+	file << res;
+	file.close();
 }
 
 void init() {
-	arr = new long long * [MAX];
-	for(int i = 0; i < MAX; i++)
-		*(arr + i) = new long long[MAX];
+	//khoi tao mang co max phan tu
+	arr = new long long * [n + 1];
+	for(int i = 0; i <= n; i++)
+		*(arr + i) = new long long[k + 1];
 		
 	
-	for(int i = 0; i < MAX; i++) {
-		for(int j = 0; j < MAX; j++) {
+	//khoi tao bang
+	//C(i, j)
+	for(int i = 0; i <= n; i++) {
+		for(int j = 0; j <= k; j++) {
 			arr[i][j] = -1;
 		}
 		arr[i][i] = 1;
 		arr[i][0] = 1;
-	}
-	
+	}	
 }
 
+//giai phong vung nho
 void dispose() {
 	delete [] arr;
 }
 
-long long dynToHop(int k, int n) {
-//	if (k > n)
-//		arr[n][k] = 0;
-//	else if (k == n)
-//		arr[n][k] = 1;
-	if (arr[n][k] != -1)
-		return arr[n][k];
-	else 
-		arr[n][k] = dynToHop(k - 1, n - 1) + dynToHop(k, n - 1);
-	return arr[n][k];
-}
 
-long long dynToHop2(int k, int n) {
-	for(int i = 2; i <= n; i++) {
-		for(int j = 1; j < i; j++) {
+long long dynToHop2(int n, int k) {
+	for(int j = 1; j <= k; j++) {
+		for(int i = j + 1 ; i <= n - k + j; i++) {
 			arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
 		}
 	}
 	return arr[n][k];
 }
 
-
-
 int main() {
+	loadData();
 	init();
-	//long long res = toHop(20, 30);
-	long long res = dynToHop2(20, 30);
-	cout << res << endl;
+	res = dynToHop2(n ,k);
+	cout << "C(" << n << ", " << k << ") = " << res << endl;
+	writeResult();
 	dispose();
 	return 0;
 }
