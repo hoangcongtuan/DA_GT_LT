8b412fe24593c0bebf0427bc2f689fc5e9a6b57c
CongTuan <congtuanspm97@gmail.com>
2017-11-25 16:42:38 +0700

    Hoan Thanh

--- BaLo/main.cpp
+++ BaLo/main.cpp
@@ -1,18 +1,51 @@
 #include <iostream>
 #include <math.h>
 #include <iomanip>
-#define N 5
-#define MAX_W 13
+#include <fstream>
+#define gach_ngang 196
+#define chu_thap 197
+#define gach_doc 179
 
 using namespace std;
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int khoi_luong[N + 1] = {0, 3, 4, 5, 2, 1};
-int gia_tri[N + 1] = {0, 4, 5, 6, 3, 1};
 
-int table[N + 1][MAX_W + 1];
+const char * FILE_INPUT = "input.txt";
+const char * FILE_OUTPUT = "output.txt";
+int N;
+int MAX_W;
+int *khoi_luong;
+int *gia_tri;
 
-void init() {
+int **table;
+
+void loadData() {
+	ifstream ifsInput;
+	ifsInput.open(FILE_INPUT, ios::in);
+	if (!ifsInput.is_open()) {
+		cout << "Loi khi mo file! Exit!";
+		exit(0);
+	}
+	
+	ifsInput >> N;
+	ifsInput >> MAX_W;
+	
+	khoi_luong = new int [MAX_W + 1];
+	gia_tri = new int [N + 1];
+	
+	table = new int *[N + 1];
+	for(int i = 0; i <= N; i++) {
+		*(table + i) = new int[MAX_W + 1];
+	}
+	
+	khoi_luong[0] = 0;
+	gia_tri[0] = 0;
+	//read data from file
+	for(int i = 1; i <= N; i++) {
+		ifsInput >> khoi_luong[i];
+		ifsInput >> gia_tri[i];
+	}
+	
 	//khoi tao cot 0
 	for(int i = 0; i <= N; i++) {
 		table[i][0] = 0;
@@ -22,6 +55,8 @@ void init() {
 	for(int i = 0; i <= MAX_W; i++) {
 		table[0][i] = 0;
 	}
+	
+	ifsInput.close();
 }
 
 //giai thuat quy hoach dong
@@ -51,6 +86,64 @@ int balo() {
 	return table[N][MAX_W];
 }
 
+void writeResult() {	
+	int pa[N];
+	int w = MAX_W;
+	int n = N;
+	int count = 0;
+	
+	fstream ofsOutput;
+	ofsOutput.open(FILE_OUTPUT, ios::out);
+	if (!ofsOutput.is_open()) {
+		cout << "Loi khi mo file! Exit!";
+		exit(0);
+	}
+ 	
+	//in bang
+	for(int k = 0; k <= N; k++) {
+		for(int w = 0; w <= MAX_W; w++)
+			ofsOutput << setw(3) << table[k][w];
+		ofsOutput << endl;
+	}
+	
+	int res = table[N][MAX_W];
+	ofsOutput << "Gia tri toi da la: " << res << endl;
+	
+	//in phuong an 
+	
+
+	while(n != 0) {
+		if (table[n][w] != table[n - 1][w]) {
+			pa[count] = n;
+			count++;
+			w = w - khoi_luong[n];
+			n--;			
+		}
+		else {
+			n--; 
+		}
+	}	
+	
+	//in tieu de cua bang
+	ofsOutput << setw(10) << "Do vat" 
+		<< setw(15) << "Khoi luong" << setw(10) << "Gia tri" << endl;
+	ofsOutput << endl;
+	for(int i = 0; i < count; i++) {
+		ofsOutput << setw(10) << pa[i]
+			<<  setw(15) << khoi_luong[pa[i]]
+			<< setw(10) << gia_tri[pa[i]] << endl;
+	}
+	
+	ofsOutput.close();
+	
+}
+
+void printSymbol(char symbol, int count) {
+	for(int i = 0 ; i < count; i++)
+		cout << char(symbol);
+}
+
+
 void phuongAn() {
 	int pa[N];
 	int w = MAX_W;
@@ -67,14 +160,31 @@ void phuongAn() {
 			n--; 
 		}
 	}	
+	
+	//in tieu de cua bang
+	cout << setw(7) << "Do vat" << char(gach_doc) 
+		<< setw(10) << "Khoi luong" << char(gach_doc) << setw(8) << "Gia tri" << endl;
+	printSymbol(gach_ngang, 7);
+	cout << char(chu_thap);
+	printSymbol(gach_ngang, 10);
+	cout << char(chu_thap);
+	printSymbol(gach_ngang, 8);
+	cout << endl;
 	for(int i = 0; i < count; i++) {
-		cout << pa[i] << " : " << khoi_luong[pa[i]] << " : " << gia_tri[pa[i]] << endl;
+		cout << setw(7) << pa[i] << char(gach_doc) 
+			<<  setw(10) << khoi_luong[pa[i]] << char(gach_doc) 
+			<< setw(8) << gia_tri[pa[i]] << endl;
 	}
 }
+
+
+
 int main(int argc, char** argv) {
-	init();
+	loadData();
 	int res = balo();
-	cout << res << endl;
+	cout <<"Gia tri toi da cua balo = " << res << endl;
+	cout << "Phuong an: " << endl;
 	phuongAn();
+	writeResult();
 	return 0;
 }
